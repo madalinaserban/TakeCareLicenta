bb3c95b384f4488f3f66be0d9334f45b88adabbb
Madalina <madalina_serban01@yahoo.com>
2023-02-01 13:49:54 +0200

    AnimationFragment

---
+++ app/src/main/java/com/example/licentatakecare/AnimationFragment.kt
@@ -0,0 +1,28 @@
+package com.example.licentatakecare
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AnimationFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_animation, container, false)
+        Handler(Looper.myLooper()!!).postDelayed({
+        }, 4000)
+        return view
+    }
+
+    private fun onBoardingFinished(): Boolean {
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return sharedPref.getBoolean("Finished", false)
+
+    }
+}
---
+++ app/src/main/java/com/example/licentatakecare/User.java
@@ -0,0 +1,77 @@
+package com.example.licentatakecare;
+
+public class User {
+    String first_name;
+    String last_name;
+    String mail;
+    String password;
+    String age;
+    String bloodType;
+    String gender;
+
+    public User(String first_name, String last_name, String mail, String password, String age, String bloodType, String gender) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.mail = mail;
+        this.password = password;
+        this.age = age;
+        this.bloodType = bloodType;
+        this.gender = gender;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
