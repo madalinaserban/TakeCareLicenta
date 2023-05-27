2a076abf57dcf0ccba279d23fb6999eed421688a
Madalina <madalina_serban01@yahoo.com>
2023-05-27 11:22:08 +0300

    navigation works fine

---
+++ app/src/main/java/com/example/licentatakecare/Authentication/LogoFragment.java
@@ -0,0 +1,59 @@
+package com.example.licentatakecare.Authentication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.os.Looper;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import com.example.licentatakecare.R;
+import com.example.licentatakecare.databinding.FragmentLogoBinding;
+
+public class LogoFragment extends Fragment {
+    private FragmentLogoBinding binding;
+    private SharedPreferencesHelper sharedPreferencesHelper;
+
+    private static final long DELAY_DURATION = 2000; // Delay in milliseconds
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        sharedPreferencesHelper = new SharedPreferencesHelper(requireContext());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentLogoBinding.inflate(inflater, container, false);
+        delayAndNavigate();
+        return binding.getRoot();
+    }
+
+    private void delayAndNavigate() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (sharedPreferencesHelper.isLoggedIn()) {
+                    Toast.makeText(getContext(), "Navigating to Profile", Toast.LENGTH_LONG).show();
+                    Navigation.findNavController(requireView()).navigate(R.id.action_logoFragment_to_profileFragment);
+                } else {
+                    Toast.makeText(getContext(), "Navigating to Login", Toast.LENGTH_LONG).show();
+                    Navigation.findNavController(requireView()).navigate(R.id.action_logoFragment_to_loginFragment);
+                }
+            }
+        }, DELAY_DURATION);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
+
+
--- app/src/main/java/com/example/licentatakecare/MainActivity.java
+++ app/src/main/java/com/example/licentatakecare/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.licentatakecare.Authentication.SharedPreferencesHelper;
 
@@ -16,25 +17,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        SharedPreferencesHelper sharedPreferencesHelper = new SharedPreferencesHelper(this);
-
-        if (sharedPreferencesHelper.isLoggedIn()) {
-            setContentView(R.layout.activity_main);
-          //  navigateToProfileFragment();
-            return;
-        }
-
         setContentView(R.layout.activity_main);
 
-        // Rest of your onCreate code...
     }
-
-
-
-
-
-
 
 }
 
